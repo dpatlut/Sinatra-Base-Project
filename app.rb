c0b5b85dc4fd1abed584a51abd29d55a6cf782a7
David Patlut <david@theunpack.com>
2017-10-13 12:44:42 -0400

    changed stuff

--- app.rb
+++ app.rb
@@ -4,21 +4,6 @@ require "sinatra/activerecord"
 set :database, {adapter: 'postgresql'}
 
 get '/' do
-	erb :index
-end
-
-get '/users' do
 	"Hello NYCDA"
 end
 
-get '/users/:id' do
-	"Hello NYCDA"
-end
-
-get '/polls' do
-	"Hello NYCDA"
-end
-
-get '/polls/:id' do
-	"Hello NYCDA"
-end
